061d0441c3672061725ee3915cc58f2c42abb60f
Bilal Kucuk <bilalkucuk491@gmail.com>
2022-12-01 11:11:24 +0300

    added data

---
+++ src/assets/Data/dummyContext.ts
@@ -0,0 +1,43 @@
+const dummyContext = [
+  {
+    title: 'Formelle Struktur',
+    subTitle: 'Von Naturlyrik: Gedichtsanalyse',
+    childs: [
+      {title: 'Einleitung', isStar: false},
+      {title: 'Hauptteil', isStar: false},
+      {title: 'Schluss', isStar: false},
+    ],
+    isStar: false,
+  },
+  {
+    title: 'Formaler Aufbau',
+    subTitle: 'Von Naturlyrik: Gedichtsanalyse',
+    childs: [
+      {title: 'Metrum', isStar: false},
+      {title: 'Kadenz', isStar: false},
+      {title: 'Reimschema', isStar: false},
+    ],
+  },
+  {
+    title: 'Formulierungshilfen',
+    subTitle: 'Von Naturlyrik: Gedichtsanalyse',
+    childs: [
+      {title: 'Satzanfänge', isStar: false},
+      {title: 'Beschreibung/Wirkung', isStar: false},
+      {title: 'Synonyme', isStar: false},
+      {title: 'Sprachliche Mittel', isStar: false},
+    ],
+  },
+  {
+    title: 'Epochen der Naturlyrik',
+    subTitle: 'Von Naturlyrik: Gedichtsanalyse',
+    childs: [
+      {title: 'Sturm und Drang (1773-1784)', isStar: true},
+      {title: 'Romantik (1785-1835)', isStar: false},
+      {title: 'Synonyme', isStar: false},
+      {title: 'Sprachliche Mittel', isStar: false},
+    ],
+  },
+];
+
+export default dummyContext;
--- src/components/HeaderPoster/HeaderPoster.tsx
+++ src/components/HeaderPoster/HeaderPoster.tsx
@@ -1,21 +1,60 @@
-import {View, Text,Image,StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import React from 'react';
-import { image_02 } from '../../constants/images';
+import {image_02} from '../../constants/images';
 const HeaderPoster = () => {
   return (
-    <View style={styles.container} >
+    <View style={styles.container}>
       <Image style={styles.image} source={image_02} />
+      <View style={styles.contextContainer}>
+        <Text style={styles.text_01}>Naturlyrik: Gedichtsanal</Text>
+        <Text style={styles.text_02}>Deutsch</Text>
+        <Text style={styles.text_03}>LOCKER PLAYLIST</Text>
+      </View>
     </View>
   );
 };
 
-
 const styles = StyleSheet.create({
-  container:{
-    marginTop:10,
+  container: {
+    marginTop: 10,
+    flexDirection: 'row',
+    marginHorizontal: 15,
   },
-  image:{
+  contextContainer: {
+    marginLeft: 10,
+  },
+  image: {
+    width: 126,
+    height: 126,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+
+    elevation: 7,
+  },
+  text_01: {
+    marginTop: 25,
+    fontWeight: '500',
+    fontSize: 19,
+    color: '#000',
+  },
+  text_02: {
+    lineHeight: 30,
+    fontWeight: '500',
+    fontSize: 22,
+    color: '#007AFE',
+  },
+  text_03: {
+    lineHeight: 18,
+    color:"#898A8D",
+    fontWeight:"700",
+    fontSize:11,
     
-  }
-})
+  },
+});
 export default HeaderPoster;
